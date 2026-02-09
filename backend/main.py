e001423a8f0937926ddfe2029ee2f01ed53d9ea8
ishaangera <93371632+ishaangera@users.noreply.github.com>
2026-02-09 18:48:40 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -30,11 +30,13 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://moneycontrolcricpulse.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 @lru_cache(maxsize=1)
